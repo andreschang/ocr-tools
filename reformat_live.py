59a4f6d4c6620b5b5ca5b05d5665a568fcb19b26
andreschang <andres_chang@brown.edu>
2018-05-30 00:02:15 -0400

    unhelpful first examples

---
+++ reformat_live.py
@@ -0,0 +1,28 @@
+from reformat_data import *
+
+# get_latlon_indices(lat, lon, lat_bounds = [25,50], lon_bounds = [-30, 30], print_summary = True,
+#   lon_bound_convert = True)
+
+# spatial_average('aice', 2018, 2020, lat_bounds=[75,85], lon_bounds = [-30,30])
+# spatial_average('TS', 2018, 2020, lat_bounds=[75,85], lon_bounds = [-30,30])
+
+# reformat_data('TS', 2012, 2020)
+# print(reformat_summary('aice', 1946, 1950, ['time', 'lat', 'lon'], ndiv = 365))
+# spatial_average('TS', 2014, 2020, lat_bounds=[-18,-4], lon_bounds = [-80,-73])
+# reformat_data('aice', 1946, 1950, cesm_hemisphere = 'nh')
+
+# reformat_data('PRECT', 2014, 2020, dt = 'daily', mode = 'cesm')
+
+# first_look('/Volumes/Samsung_T5/Open_Climate_Research-Projects/data/raw/air.mon.mean.nc')
+
+# reformat_data('air', 1948, 2018, dt = 'monthly', mode = 'custom', \
+#   file = '/Volumes/Samsung_T5/Open_Climate_Research-Projects/data/raw/air.mon.mean.nc',\
+#   output = 'date', custom_tags = ['ncep_re'])
+
+# spatial_average('air', 2015, 2017, dt = 'monthly', mode = 'custom', lat_bounds = [50, 70], \
+#   lon_bounds = [1, 30])
+
+# reformat_data('aice', 2012, 2071, dt = 'daily', mode = 'cesm')
+
+# spatial_average('TS', 2018, 2030, dt = 'daily', mode ='cesm', lat_bounds = [20,28],\
+#   lon_bounds = [44,48])
