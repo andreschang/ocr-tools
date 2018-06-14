57b3a9c3da6d7e57484b17e73d658eceb09a122a
andreschang <andres_chang@brown.edu>
2018-06-13 22:17:20 -0400

    demo script and data folders

--- ocrtools/ocrtools/stage.py
+++ ocrtools/ocrtools/stage.py
@@ -41,6 +41,8 @@ class stage(object):
       "scratch": []}
       import matplotlib
       matplotlib.use('Agg')
+    elif preset == 'demo':
+      
     self.naming = {"cesm-reformatted": ["var_name", "dt", "cesm_mem", "time_slice"], \
     "other-reformatted": ["var_name", "dt", "time_slice"], "spatial_average": ["var_name", \
     "dt", "time_slice", "mean", "scratchId"], "cesm_report": ["var_name", "dt", "cesm_mem"], \
