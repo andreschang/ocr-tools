2d069a4761490d561d27307ab07e39a24e3e43f5
andreschang <andres_chang@brown.edu>
2018-08-22 19:39:47 -0400

    version 0.1

--- ocrtools/setup.py
+++ ocrtools/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ocrtools',
-      version='4.4',
+      version='0.1',
       description='Tools to improve malleability of climate data for research and visualization purposes',
       url='https://github.com/andreschang/ocr-tools',
       author='Andres Chang',
